85db1422919bb40f1b5daf0ec7552450917cda26
meitrix8208 <maurrod2001@outlook.com>
2024-01-21 15:53:58 -0500

    Refactor stream.ts and add stream2.ts for Nitro Streaming Demo

--- src/routes/api/stream.ts
+++ src/routes/api/stream.ts
@@ -1,14 +1,11 @@
 export default eventHandler((event) => {
   setResponseHeader(event, "Content-Type", "text/html; charset=utf-8");
-
   const encoder = new TextEncoder();
-
   const stream = new ReadableStream({
     async start(controller) {
       const write = (chunk: string) => {
         controller.enqueue(encoder.encode(chunk));
       };
-
       write(
         html`<!DOCTYPE html>
         <html lang="en">
@@ -24,11 +21,9 @@ export default eventHandler((event) => {
               content="Nitro Streaming Demo"
             />
             <link
-              id="favicon"
               rel="icon"
               type="image/png"
-              href=""
-            />
+              href="/favicon.png" />
             <link
               rel="preconnect"
               href="https://rsms.me/"
@@ -51,12 +46,12 @@ export default eventHandler((event) => {
                 color: #fff;
                 background: #333;
                 font-size: 1.5em;
-                padding: 2em; /* Reducir el relleno para dispositivos más pequeños */
+                padding: 2em;
                 display: flex;
                 flex-direction: column;
-                align-items: center; /* Centrar en el eje horizontal */
-                margin: 0; /* Eliminar el margen predeterminado del cuerpo */
-                text-align: center; /* Centrar el texto */
+                align-items: center;
+                margin: 0;
+                text-align: center;
               }
               a {
                 color: #fff;
@@ -66,15 +61,14 @@ export default eventHandler((event) => {
                 margin: 0.5em 0;
               }
               p {
-                max-width: 1200px; /* Ajustar el ancho máximo del texto para evitar líneas extremadamente largas */
-                margin: 0 auto; /* Centrar el párrafo horizontalmente */
-                text-align: justify; /* Centrar el texto */
-                text-justify: inter-word; /* Justificar el texto */
+                max-width: 1200px;
+                margin: 0 auto;
+                text-align: justify;
+                text-justify: inter-word;
               }
-        
               @media only screen and (max-width: 860px) {
                 body {
-                  font-size: 1.2em; /* Reducir el tamaño del texto para dispositivos más pequeños */
+                  font-size: 1.2em;
                   padding: 1em;
                 }
                 h1 {
@@ -90,25 +84,18 @@ export default eventHandler((event) => {
               >Source Code</a
             ><br /><br />
             <p>
-        
           </html> `
       );
-
-      const text = `Nitro, an open source TypeScript framework, empowers you to create web servers that run anywhere, offering a range of impressive features such as rapid development through a zero config setup with hot module replacement for server code in development, a versatile deployment capability that allows for codebase deployment to any provider without extra configuration, and a portable and compact design, effectively eliminating the need for 'node_modules' with an output size of less than 1MB. Its filesystem routing feature automatically registers server and API routes, while maintaining a minimal design to fit into any solution with minimum overhead. The framework supports asynchronous chunk loading via code-splitting for a fast server startup time and response. Inherent TypeScript support is provided with several additional enhancements. Nitro also offers a multi-driver, platform-agnostic storage system, a powerful built-in caching API, and is highly customizable through its plugins hooks system. It further enhances code clarity with an auto imports feature, which automatically imports utilities for a minimal and clean codebase, adding only the used ones to the final bundle. Remarkably, Nitro maintains backward compatibility, enabling the use of legacy npm packages, CommonJS, and mocking Node.js modules for workers. This engine, openly powering Nuxt, is accessible to all, paving the way for a versatile and user-friendly web server development experience.
-      `;
-      
+      const text = `Nitro, an open source TypeScript framework, empowers you to create web servers that run anywhere, offering a range of impressive features such as rapid development through a zero config setup with hot module replacement for server code in development, a versatile deployment capability that allows for codebase deployment to any provider without extra configuration, and a portable and compact design, effectively eliminating the need for 'node_modules' with an output size of less than 1MB. Its filesystem routing feature automatically registers server and API routes, while maintaining a minimal design to fit into any solution with minimum overhead. The framework supports asynchronous chunk loading via code-splitting for a fast server startup time and response. Inherent TypeScript support is provided with several additional enhancements. Nitro also offers a multi-driver, platform-agnostic storage system, a powerful built-in caching API, and is highly customizable through its plugins hooks system. It further enhances code clarity with an auto imports feature, which automatically imports utilities for a minimal and clean codebase, adding only the used ones to the final bundle. Remarkably, Nitro maintains backward compatibility, enabling the use of legacy npm packages, CommonJS, and mocking Node.js modules for workers. This engine, openly powering Nuxt, is accessible to all, paving the way for a versatile and user-friendly web server development experience.`;
       for (const token of text.split(" ")) {
         write(token + " ");
         await waitFor(50);
       }
-
       controller.close();
     },
   });
-
   return stream;
 });
-
 function waitFor(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
