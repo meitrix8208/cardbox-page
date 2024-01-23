6ce775bbc9f634d0945f043d2ff9ff3765f4063a
meitrix8208 <maurrod2001@outlook.com>
2024-01-22 22:50:48 -0500

    Update text animation delay

--- src/routes/api/stream2.ts
+++ src/routes/api/stream2.ts
@@ -45,7 +45,7 @@ export default eventHandler((event) => {
       `;
       for (const token of text.split(" ")) {
         write(token + " ");
-        await waitFor(50);
+        await waitFor(25);
       }
 
       controller.close();
