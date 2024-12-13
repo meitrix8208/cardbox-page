1c7bb9e4a651bc23e7159565166e94df6fd62939
meitrix8208 <maurrod2001@outlook.com>
2024-12-12 19:51:26 -0500

    feat: Enhance IP API handler to include geo information and request URL

--- server/api/ip.ts
+++ server/api/ip.ts
@@ -1,23 +1,31 @@
+import { Buffer } from "node:buffer";
+
 export default eventHandler(async (event) => {
-  const UserAgent = await getRequestFingerprint(event, {
-    userAgent: true,
-    xForwardedFor: false,
-    method: false,
-    path: false,
-    hash: false,
-    ip: false,
-  });
   const host = getRequestHost(event, { xForwardedHost: true });
   const ip = getRequestIP(event, { xForwardedFor: true });
-  const query = getQuery(event);
+  const args = getQuery(event);
   const headers = getRequestHeaders(event);
+  const url = new URL(event.node.req.url, `http://${host}`);
+  const path = event.path.split("?")[0];
+  let geo = {};
+  if (headers["x-nf-geo"]) {
+    try {
+      geo = JSON.parse(Buffer.from(headers["x-nf-geo"], "base64").toString("utf-8"));
+    }
+    catch (error) {
+      geo = { error };
+      console.error("Failed to parse geo information:", error);
+    }
+  }
+
   return {
     method: event.method,
+    url,
     ip,
+    geo,
     host,
     headers,
-    path: event.node.req.url,
-    userAgent: UserAgent,
-    args: query,
+    path,
+    args,
   };
 });
